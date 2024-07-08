279ada2f008839d35215560b6054f3f254134767
rizkilaks <rizki30laksmana@gmail.com>
2024-07-08 18:59:04 +0700

    added newsSources component

--- frontend/src/components/newsSources/NewsSources.jsx
+++ frontend/src/components/newsSources/NewsSources.jsx
@@ -1,46 +1,53 @@
 import React from "react";
 import "./newsSources.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRightLong, faRefresh } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRightLong, faEarthAmerica, faRss, faSync } from "@fortawesome/free-solid-svg-icons";
+import { faCalendarDays, faFlag, faStar } from "@fortawesome/free-regular-svg-icons";
 
 const NewsSources = () => {
   return (
     <div className="newsSources">
       <div className="nsContainer">
         <div className="nsItems">
-          <h4>Access 150,000 News Sources Worldwide</h4>
-          <h5>
-            Feed the latest and most popular news articles into your application or website, fully automated & updated
-            every minute.
-          </h5>
-          <div className="nsItemsFlex">
-            <div className="nsItemsLogo">
-              <img src="/World-White.png" alt="" />
-              <FontAwesomeIcon icon={faArrowRightLong} />
-            </div>
-            <div className="nsItemsList">
-              <ul>
-                <li>
-                  <FontAwesomeIcon icon={faRefresh} />
-                  13 Languages
-                </li>
-                <li>
-                  <FontAwesomeIcon icon={faRefresh} />
-                  13 Languages
-                </li>
-                <li>
-                  <FontAwesomeIcon icon={faRefresh} />
-                  13 Languages
-                </li>
-                <li>
-                  <FontAwesomeIcon icon={faRefresh} />
-                  13 Languages
-                </li>
-                <li>
-                  <FontAwesomeIcon icon={faRefresh} />
-                  13 Languages
-                </li>
-              </ul>
+          <div className="nsItemsContainer">
+            <h4>Access 150,000 News Sources Worldwide</h4>
+            <h5>
+              Feed the latest and most popular news articles into your application or website, fully automated & updated
+              every minute.
+            </h5>
+            <div className="nsItemsFlex">
+              <div className="nsItemsLogo">
+                <img src="/World-White.png" alt="" />
+                <FontAwesomeIcon icon={faArrowRightLong} />
+              </div>
+              <div className="nsItemsList">
+                <ul>
+                  <li>
+                    <FontAwesomeIcon icon={faSync} />
+                    Real-Time News Data
+                  </li>
+                  <li>
+                    <FontAwesomeIcon icon={faCalendarDays} />
+                    Historical News Data
+                  </li>
+                  <li>
+                    <FontAwesomeIcon icon={faStar} />
+                    Headline News
+                  </li>
+                  <li>
+                    <FontAwesomeIcon icon={faRss} />
+                    150,000+ News Sources
+                  </li>
+                  <li>
+                    <FontAwesomeIcon icon={faEarthAmerica} />
+                    54 Countries
+                  </li>
+                  <li>
+                    <FontAwesomeIcon icon={faFlag} />
+                    14 Languages
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
